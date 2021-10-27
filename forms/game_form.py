c0f4c8ebd079f02bc863b8eb9d8dea2ef042e30b
Jarki <yaroslav.ivashko@gmail.com>
2021-10-27 13:43:23 +0300

    altered the form

--- forms/game_form.py
+++ forms/game_form.py
@@ -2,40 +2,54 @@ from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout
 from PyQt5.QtWidgets import QLabel, QListWidget, QLineEdit, QPushButton
 from PyQt5.QtCore import Qt
 
+
 class GameForm:
     def __init__(self, container_widget):
         self.container = container_widget
 
-        print(self.container.geometry())
-        width = 1080
-        height = 720
+        width = self.container.geometry().width()
+        height = self.container.geometry().height()
 
+        # wrapper for all widgets in the form
+        self.wrapper = QVBoxLayout()
+        self.container.setLayout(self.wrapper)
+
+        # game info container
         self.game_info_container = QHBoxLayout()
-        self.container.setLayout(self.game_info_container)
 
+        # container for all of the info about player
         self.player_info_container = QVBoxLayout()
-        self.game_info_container.addLayout(self.player_info_container)
 
+        # labels
         self.ping_label = QLabel(text='ping')
         self.server_address_label = QLabel('server-addr')
         self.opponent_info_label = QLabel('opponent')
-        self.game_log = QListWidget()
-        self.game_log.setGeometry(0, 0, 100, 100)
-        self.game_log.addItem('asas')
-        self.game_log.setMaximumWidth(int(width / 2))
-        self.game_log.setMaximumHeight(int(height / 2))
 
         self.player_info_container.addWidget(self.ping_label)
         self.player_info_container.addWidget(self.server_address_label)
-        self.game_info_container.addWidget(self.game_log)
+        self.player_info_container.addWidget(self.opponent_info_label)
 
+        # list widget to keep track of player actions
+        self.game_log = QListWidget()
+        self.game_log.setGeometry(0, 0, 100, 100)
+        self.game_log.setMaximumWidth(int(width / 2))
+        self.game_log.setMaximumHeight(int(height / 2))
+
+        # place everything
+        self.game_info_container.addLayout(self.player_info_container)
+        self.game_info_container.addWidget(self.game_log)
+        self.wrapper.addLayout(self.game_info_container)
+
+        # create a container for inputs
         self.game_action_container = QVBoxLayout()
         self.game_action_container.setAlignment(Qt.AlignCenter)
 
+        # create inputs
         self.message_input = QLineEdit()
         self.message_input.setMaximumWidth(256)
         self.message_button = QPushButton('Send message')
 
         self.game_action_container.addWidget(self.message_input)
         self.game_action_container.addWidget(self.message_button)
-        self.game_info_container.addLayout(self.game_action_container)
+
+        self.wrapper.addLayout(self.game_action_container)
